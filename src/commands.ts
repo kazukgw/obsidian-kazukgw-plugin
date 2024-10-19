37492e3a7aaaa622121e1d633dc54975e8985f8a
Kazuya Kagawa <kazukgw@gmail.com>
2024-10-19 15:36:43 +0900

    不要なコマンドを削除

--- src/commands.ts
+++ src/commands.ts
@@ -4,12 +4,6 @@ import { AddTagPropertyCommand } from "command/addTagProperty";
 import { CreateMemoFromSelectionCommand } from "command/createMemoFromSelection";
 import { CreateNoteFromSelectionCommand } from "command/createNoteFromSelection";
 import { CreateQuotedNoteFromSelectionCommand } from "command/createQuotedNoteFromSelection";
-import { CreateChatAISessionFileCommand } from "command/createChatAISessionFile";
-import { TranslateToJapaneseCommand } from "command/translateToJapanese";
-import { SummarizeCommand } from "command/summarize";
-import { DescribeConceptCommand } from "command/describeConcept";
-import { DescribeEnglishWardCommand } from "command/describeEnglishWard";
-import { DescribeEnglishSentenceCommand } from "command/describeEnglishSentence";
 import { AddLinkToDailyNoteCommand } from "command/addLinkToDailyNote";
 import { AddBlockLinkToDailyNoteCommand } from "command/addBlockLinkToDailyNote";
 import { CreateDailyNoteViewer } from "command/createDailyNoteViewer";
@@ -21,12 +15,6 @@ export const commands: CommandWithContext[] = [
     new CreateMemoFromSelectionCommand(),
     new CreateNoteFromSelectionCommand(),
     new CreateQuotedNoteFromSelectionCommand(),
-    new CreateChatAISessionFileCommand(),
-    new TranslateToJapaneseCommand(),
-    new SummarizeCommand(),
-    new DescribeConceptCommand(),
-    new DescribeEnglishWardCommand(),
-    new DescribeEnglishSentenceCommand(),
     new AddLinkToDailyNoteCommand(),
     new AddBlockLinkToDailyNoteCommand(),
     new CreateDailyNoteViewer(),
